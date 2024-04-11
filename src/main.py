58eed065292205ac0e9796a7cff473d27aac3229
Michael Dyer <michaelowendyer@gmail.com>
2024-04-11 17:26:54 +0200

    Move Python into src directory

--- src/main.py
+++ src/main.py
@@ -1,39 +1,5 @@
 import os
-from distributions import (
-    beta,
-    binomial,
-    cauchy,
-    chi_squared,
-    dirichlet,
-    exponential,
-    exponential_exp1,
-    fisher_f,
-    frechet,
-    gamma,
-    geometric,
-    gumbel,
-    hypergeometric,
-    inverse_gaussian,
-    log_normal,
-    normal,
-    normal_inverse_gaussian,
-    pareto,
-    pert,
-    poisson,
-    skew_normal,
-    standard_geometric,
-    standard_normal,
-    student_t,
-    triangular,
-    unit_ball,
-    unit_circle,
-    unit_disc,
-    unit_sphere,
-    weibull,
-    zeta,
-    zipf,
-)
-
+from distributions import frechet
 
 if __name__ == "__main__":
     out = "charts"
@@ -49,7 +15,7 @@ if __name__ == "__main__":
         # exponential,
         # exponential_exp1,
         # fisher_f,
-        frechet,
+            frechet,
         # gamma,
         # geometric,
         # gumbel,
