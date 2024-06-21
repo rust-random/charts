3ba76f10fdd195a2c1a1d49c714edcc1eb570927
Michael Dyer <59163924+MichaelOwenDyer@users.noreply.github.com>
2024-06-21 23:17:03 +0200

    Update Definition of Geometric distribution (#5)
    * Plot for the Geometric distribution has been updated to show the number of failures before first success instead of the number of trials until first success

--- src/distributions/geometric.py
+++ src/distributions/geometric.py
@@ -7,7 +7,7 @@ def save_to(directory: str, extension: str):
     # Possible values of p for the distribution
     p_values = [0.2, 0.5, 0.8]
     # Possible outcomes for a Geometric distributed variable
-    outcomes = np.arange(1, 11)
+    outcomes = np.arange(0, 10)
 
     # Creating the figure and the axis
     fig, ax = plt.subplots()
@@ -15,11 +15,13 @@ def save_to(directory: str, extension: str):
 
     # Plotting the PMF for each value of p
     for i, p in enumerate(p_values):
-        ax.bar(outcomes + i * width - width, geom.pmf(outcomes, p), width=width, label=f'p = {p}')
+        # Specify loc=-1 correct for the difference in the definition of the geometric distribution in scipy
+        # and the definition in the rand crate
+        ax.bar(outcomes + i * width - width, geom.pmf(outcomes, p, loc=-1), width=width, label=f'p = {p}')
 
     # Adding title and labels
     ax.set_title('Geometric distribution')
-    ax.set_xlabel('Number of trials until first success')
+    ax.set_xlabel('Number of failures before first success')
     ax.set_ylabel('Probability')
     ax.set_xticks(outcomes)  # set the ticks to be the outcome values
 
