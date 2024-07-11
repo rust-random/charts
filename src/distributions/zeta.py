9b6519c2992cf2d9bc448e884987c18b854b60ae
Michael Dyer <michaelowendyer@gmail.com>
2024-07-11 12:57:50 +0200

    Rename Zeta parameter from a to s

--- src/distributions/zeta.py
+++ src/distributions/zeta.py
@@ -4,16 +4,16 @@ from scipy.stats import zipf
 
 
 def save_to(directory: str, extension: str):
-    inputs = [3, 2, 1.5, 1.1]  # Different values of s and n
+    inputs = [3, 2, 1.5, 1.1]  # Different values of s
     outcomes = np.arange(1, 11)  # Outcomes from 1 to 10
 
     # Creating the figure
     fig, ax = plt.subplots()
     width = 0.2  # Bar width
 
-    # Plotting the Zipf Distribution for each value of s
-    for i, a in enumerate(inputs):
-        ax.bar(outcomes + i * width - width * 3 / 2, zipf.pmf(outcomes, a), width=width, label=f'a = {a}')
+    # Plotting the Zeta Distribution for each value of s
+    for i, s in enumerate(inputs):
+        ax.bar(outcomes + i * width - width * 3 / 2, zipf.pmf(outcomes, s), width=width, label=f's = {s}')
 
     ax.set_title('Zeta Distribution')
     ax.set_xlabel('Outcome')
